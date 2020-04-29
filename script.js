0d4b2acc236743c5940bc35863df5518748337b4
unknown <stoichevrado@gmail.com>
2020-04-29 18:00:01 +0300

    second commit

--- script.js
+++ script.js
@@ -2,18 +2,27 @@ const strengthMeter = document.getElementById('strength-meter');
 const passwordInput = document.getElementById('password-input');
 const reasonsContainer = document.getElementById('reasons');
 
-passwordInput.addEventListener('input',updateStrMeter)
+passwordInput.addEventListener('input',updateStrMeter);
+updateStrengthMeter()
+
 const updateStrMeter= () => {
     const weaknesses = calculatePassStr(passwordInput.value);
     let strength = 100;
+    reasonsContainer.innerHTML = '';
     weaknesses.forEach(weakness => {
+        if(weakness == null) return
         strength -= weakness.deduction
+        const messageElement = document.createElement('div')
+        messageElement.innerText = weakness.message
+        reasonsContainer.appendChild()
     })
     strengthMeter.style.setProperty('--strength', strength)
 }
 const calculatePassStr = (password) =>{
     const weaknesses = [];
-    weaknesses.push(lengthWeaknes(password))
+    weaknesses.push(lengthWeaknes(password));
+    weaknesses.push(lowercaseWeakness(password));
+    weaknesses.push(uppercaseWeakness(password));
     return weaknesses
 }
 const lengthWeaknes = (password) =>{
@@ -32,4 +41,25 @@ const lengthWeaknes = (password) =>{
         }
     }
 
+}
+const lowercaseWeakness = (password) =>{
+   return characterTypeWeakness(password, /[a-z]/g, 'lowercase characters')
+}
+const uppercaseWeakness = (password) => {
+    return characterTypeWeakness(password, /[A-Z]/g, 'uppercase characters')
+}
+const characterTypeWeakness = (password, regex, type) =>{
+    const match = password.match(regex) || [];
+    if(match.length === 0){
+        return{
+            message: `Your password has no ${type}`,
+            deduction: 20
+        }
+    }
+    if(match.length <= 2){
+        return{
+            message: `Your password could use more ${type}`,
+            deduction: 5
+        }
+    }
 }
